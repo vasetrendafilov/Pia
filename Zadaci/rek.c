d2fdf0fac531dde595ab5e3c725501e378f60cb6
Никола Стоименов <nstoimenov06@outlook.com>
2018-11-17 19:22:47 +0100

    Kolokviumski

---
+++ Zadaci/rek.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void rekurzija(int x, char z){
+  if (x > 0){
+    rekurzija(x/10, z);
+    printf("%c", z + (x%10));
+  }
+}
+
+int main(){
+  int broj = 0;
+  char bukva = ' ';
+
+  printf("Vnesi Broj:\n");
+  scanf("%d", &broj);
+
+  printf("Vnesi bukva:\n");
+  scanf(" %c", &bukva);
+
+  rekurzija(broj, bukva);
+  return 0;
+}
