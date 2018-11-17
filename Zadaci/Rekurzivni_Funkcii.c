9ad0047bac1cdfb6ac5c77bea4c4238281250684
Никола Стоименов <nstoimenov06@outlook.com>
2018-11-17 20:54:58 +0100

    Rekurzivni (po prosto malce od na Vase)

---
+++ Zadaci/Rekurzivni_Funkcii.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/* Avtor: Nikola Stoimenov 
+   Rekurzivni funkcii za odredeni matemtichki izrazi
+   *Napomena funkciite zavisat edna od druga
+   *Ushte edna napomena nema da go razberete ova XD */
+
+//Broj na cifri vo eden broj (Rekurzivno)
+int brojCifri(int x){
+  if (x == 0) return 0;
+  return 1 + brojCifri(x/10);
+}
+
+//Zbir na cifrite vo brojot (Rekurzivno)
+int zbirCifri(int x){
+  if (x == 0) return 0;
+  return (x % 10) + zbirCifri(x/10);
+}
+
+//Stepenuvanje na broj (Rekurzivno)
+int stepen(int x, int y){
+  if (y == 0) return 1;
+  return x * stepen(x, y-1);
+}
+
+//prevrtuvanje na cifri (rekurzivno)
+int prevrtiCifri(int x){
+    if (x == 0) return 0;
+    return x%10 * stepen(10, brojCifri(x)-1) + prevrtiCifri(x/10);
+}
+
+int main(){
+  printf("%d\n", prevrtiCifri(158));
+  return 0;
+}
