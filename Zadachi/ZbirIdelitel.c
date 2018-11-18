d6cbf30dc99cf1db89204a9cc26e0d303f26734d
Vasil Trendafilov <vasetrendafilov@users.noreply.github.com>
2018-11-18 14:47:18 +0100

    s

--- Zadachi/ZbirIdelitel.c
+++ Zadachi/ZbirIdelitel.c
@@ -1,5 +1,5 @@
 /*
-Zadaca za vezbanje kolokviumska: 
+Zadaca za vezbanje kolokviumska:
 TEZINA: **(*****)
 Od standarden vlez se vnesuvaat celi broevi A,B,M
 Da se ispechati zbirot na  prosti broevi koi se naogjaat vo opseg [A,B]. Taka sto dopolnitelno da se ispechati DA ili NE
@@ -25,26 +25,25 @@ OUTPUT: 533 NE
 int main()
 {
    int A,B,M;// INPUT
-   int deliteli; //INTERFACE 
+   int deliteli; //INTERFACE
    int sum=0; // OUTPUT
    scanf("%d %d %d",&A,&B,&M);
-   
+
    for(; A<=B; A++)
    {
-       
+
        int flag=0; // A e prost broj
        for(int i=2; i<=A/2; i++)
           if(!(A%i)){ flag=1; break;} // flag= 1 , znaci A ne e prost broj
-         
-         sum+=(!flag)? A : 0; 
+
+         sum+=(!flag)? A : 0;
     }
     printf("%d ",sum);
-    
+
     deliteli=2; //sekoj broj ima minimum 2 deliteli
      for(int i=2; i<=sum/2; deliteli= (!(sum%i)) ? deliteli+1 : deliteli, i++);
-     
+
      printf((deliteli>M)?"DA":"NE");
 
     return 0;
 }
-
