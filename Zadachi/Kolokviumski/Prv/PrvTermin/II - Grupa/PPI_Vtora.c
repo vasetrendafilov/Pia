a8e2f3bd42f45d655fef35ef1bb924f6898f4806
Никола Стоименов <nstoimenov06@outlook.com>
2018-11-18 15:48:38 +0100

    Преуредување, ППИ

---
+++ Zadachi/Kolokviumski/Prv/PrvTermin/II - Grupa/PPI_Vtora.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+  unsigned int broj, a = 0;
+  printf("Vnesuvaj dvocifreni broevi (Trocifren broj ili znak, za stop):\n");
+  while(scanf("%u", &broj) && broj < 100){
+    if (broj > 9) a = a*100 + broj%100;
+    else a = a*10 + broj%10;
+  }
+  printf("Poveli: %u", a);
+  return 0;
+}
